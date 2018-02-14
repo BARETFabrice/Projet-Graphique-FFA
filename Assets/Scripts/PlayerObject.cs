36c72d672c26e0b066e5585c963098d9742be7e1
fsimoneau <fsimoneau997@gmail.com>
2018-02-14 12:49:18 -0500

    Test du network

---
+++ Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (PlayerUnitPrefab);
+	}
+
+	public GameObject PlayerUnitPrefab;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
