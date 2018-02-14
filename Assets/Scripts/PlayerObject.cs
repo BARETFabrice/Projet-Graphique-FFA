464cb41d00516c98b9fd0154e1308ab9a1fef0b4
fsimoneau <fsimoneau997@gmail.com>
2018-02-14 14:13:13 -0500

    Networking

--- Assets/Scripts/PlayerObject.cs
+++ Assets/Scripts/PlayerObject.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerObject : MonoBehaviour {
+public class PlayerObject : NetworkBehaviour {
+
 
 	// Use this for initialization
 	void Start () {
-		Instantiate (PlayerUnitPrefab);
+		if (isLocalPlayer == false) {
+			return;
+		}
+			
+		//UnitInstance = Instantiate (PlayerUnitPrefab);
+		CmdSpawnMyUnit();
 	}
 
 	public GameObject PlayerUnitPrefab;
 
 	// Update is called once per frame
 	void Update () {
-		
+		if (isLocalPlayer == false) {
+			return;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			CmdMoveUnitUp ();
+		}
 	}
+
+	GameObject myPlayerUnit;
+
+	[Command]
+	void CmdSpawnMyUnit(){
+		GameObject go = Instantiate (PlayerUnitPrefab);
+
+		myPlayerUnit = go;
+
+		NetworkServer.Spawn (go);
+	}
+
+	[Command]
+	void CmdMoveUnitUp()
+	{
+		if (myPlayerUnit == null) {
+			return;
+		}
+		myPlayerUnit.transform.Translate (0, 1, 0);
+	}
+
+	/*void OnDestroy(){
+		Object.Destroy (UnitInstance);
+	}*/
 }
