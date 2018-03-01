3c3032c5e1d7c965609d8719f34e352362f8a6c1
alex007sirois <alex007sirois@gmail.com>
2018-03-01 16:55:58 -0500

    fix crosshair + début timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    float timeLeft = 3600;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        timeLeft -= Time.deltaTime;
+        Debug.Log(timeLeft);
+    }
+}
