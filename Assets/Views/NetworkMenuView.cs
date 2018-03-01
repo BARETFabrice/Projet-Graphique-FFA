357c049cf8ca29a8ab7215b9e87fe77729e5ccf4
fsimoneau <fsimoneau997@gmail.com>
2018-03-01 17:03:05 -0500

    Network view script

---
+++ Assets/Views/NetworkMenuView.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkMenuView : MonoBehaviour
+{
+	
+    public void OnButtonPlayClicked()
+    {
+        MainMenuController.LoadPlayScene();
+    }
+
+	public void OnButtonQuitClicked()
+	{
+		MainMenuController.QuitGame();
+	}
+}
