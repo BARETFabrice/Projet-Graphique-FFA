422a2e83019c604f89c85138b6f86901149d826c
alex007sirois <alex007sirois@gmail.com>
2018-03-07 13:45:11 -0500

    avancement du timer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameManager : NetworkBehaviour {
+
+    float timeLeft;
+
+    public string getTimer()
+    {
+        int minutes = (int)(timeLeft / 60);
+        int seconds = (int)(timeLeft - (minutes * 60));
+
+        return "" + minutes + ":" + seconds;
+    }
+
+    [Command]
+    void CmdSyncTimeLeft()
+    {
+        RpcSyncTimeLeft(timeLeft);
+    }
+
+    [ClientRpc]
+    void RpcSyncTimeLeft(float timeLeft)
+    {
+        setTime(timeLeft);
+    }
+
+    void setTime(float time)
+    {
+        timeLeft = time;
+    }
+
+    // Use this for initialization
+    void Start () {
+        setTime(600);
+
+        if (isServer)
+            InvokeRepeating("CmdSyncTimeLeft", 1.0f, 50F);
+    }
+
+    [Command]
+    void CmdEndGame()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+
+        if (isServer && timeLeft < 0)
+            CmdEndGame();
+    }
+}
--- Assets/Views/HUDView.cs
+++ Assets/Views/HUDView.cs
@@ -1,15 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Crosshair : MonoBehaviour {
+public class HUDView : MonoBehaviour {
 
     public Texture2D crosshairImage;
+    public Text timer;
 
     void OnGUI()
+    {
+        drawCrosshair();
+    }
+
+    void drawCrosshair()
     {
         float xMin = (Screen.width / 2) - (crosshairImage.width / 2);
         float yMin = (Screen.height / 2) - (crosshairImage.height / 2);
         GUI.DrawTexture(new Rect(xMin, yMin, crosshairImage.width, crosshairImage.height), crosshairImage);
     }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
